3444fce0a44a320f3eba978e0ffd12a62a9d0b49
Gong-KYH <gong.moonphruk@student.kyh.se>
2021-09-23 21:32:50 +0200

    Initial commit

---
+++ Todo.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_and_Switch
+{
+    class Todo
+    {
+        public string Description { get; set; }
+        public int EstimatedHours { get; set; }
+        public Status Status { get; set; }
+
+    }
+
+    enum Status
+    {
+        NotStarted,
+        InProgress,
+        OnHold,
+        Completed,
+        Deleted
+    }
+}
